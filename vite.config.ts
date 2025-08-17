ead62a4dea58af98a521129dd2d4479e5c534f73
Anishcharith Bhandary <anishcb@amazon.com>
2025-08-17 15:52:48 +0530

    page url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Replace with your repo name
-const repoName = "photo-matcher";
+const repoName = "photo_matcher";
 
 export default defineConfig({
   plugins: [react()],
