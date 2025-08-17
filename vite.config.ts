d02450ceb6d763d1350977c419bbf9813d880d9c
Anishcharith Bhandary <anishcb@amazon.com>
2025-08-17 15:44:21 +0530

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Replace "my-vision-app" with your repo name when deploying to GitHub Pages
+// Replace with your repo name
+const repoName = "photo-matcher";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/my-vision-app/"
-})
+  base: `/${repoName}/`,  // 👈 this is important
+});
